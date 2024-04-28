87a124549fece9433022f0ed141849f45323a66d
Santosh Barai <barai3022@gmail.com>
2024-04-28 19:56:42 +0600

    1st

---
+++ src/utility/EmailUtility.js
@@ -0,0 +1,20 @@
+import nodemailer from 'nodemailer'
+
+export async function SendEmail(EmailTo, EmailText, EmailSubject){
+    let Transport = nodemailer.createTransport({
+        host:"mail.teamrabbil.com",
+        post:25,
+        secure:false,
+        auth:{user:"info@teamrabbil.com", pass:"~sR4[bhaC[Qs"},
+        tls: {rejectUnauthorized:false}
+    })
+
+    let MailOption ={
+        from:"Next Ecommerce<info@teamrabbil.com>",
+        to:EmailTo,
+        subject:EmailSubject,
+        text:EmailText
+    }
+
+    return await Transport.sendMail(MailOption)
+}
